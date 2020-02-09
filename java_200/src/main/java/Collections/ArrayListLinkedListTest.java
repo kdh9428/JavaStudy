ff60a308f1c65986736b46f293f15619360d7715
kdh9428 <kim23222@naver.com>
2020-02-09 16:56:38 +0900

    ArrayList와 LinkedList 접근 시간 테스트

--- java_200/src/main/java/Collections/ArrayListLinkedListTest.java
+++ java_200/src/main/java/Collections/ArrayListLinkedListTest.java
@@ -1,10 +1,34 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ArrayListLinkedListTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList al = new ArrayList(20000);
+		LinkedList ll = new LinkedList();
+		
+		add(al);
+		add(ll);
+		
+		System.out.println(" = 접근 시간 테스트 = ");
+		System.out.println("ArrayList : "+access(al));
+		System.out.println("ArrayList : "+access(ll));
+		}
+	public static void add(List list) {
+		for(int i=0;i<100000;i++) list.add(i+"");
+	}
+	public static long access(List list) {
+		long start = System.currentTimeMillis();
+		
+		for(int i =0; i<10000; i++) list.get(i);
+		
+		long end = System.currentTimeMillis();
+		
+		return end - start;
 	}
 
 }
