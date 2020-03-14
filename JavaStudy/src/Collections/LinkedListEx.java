29f59581635614d91615490ee79b5cbe898b0c06
kdh9428 <kim23222@naver.com>
2020-03-14 23:55:50 +0900

    [추가] LinkedListEx - addLast,node,add 메소드

--- JavaStudy/src/Collections/LinkedListEx.java
+++ JavaStudy/src/Collections/LinkedListEx.java
@@ -8,8 +8,6 @@ public class LinkedListEx {
 		head.append(2);
 		head.append(3);
 		head.append(4);
-		head.delete(2);
-		head.delete(3);
 		head.retvieve();
 		System.out.println(head+"확인합니다. 주소를~~");
 	}
@@ -37,7 +35,6 @@ class Node{
 		while(n.next != null) {
 			if(n.next.date == d) {
 				n.next = n.next.next;
-				System.out.println(n.next+"==="+n.next.next);
 			}else {
 				n = n.next;
 			}
@@ -85,7 +82,42 @@ class LinkedList1{
 		if(head.next == null) {//head의 다음 값이 없다면
 			tail = head;//첫번째 생성된 객체를 헤드로 지정한다.
 		}
+	}
+	
+	public void addLast(Object input) {
+		Node1 newNode= new Node1(input);
+		if(size == 0) {
+			addFirst(input);
+		}else {
+			tail.next = newNode;
+			tail = newNode;
+			size++;
+		}
+	}
+	
+	Node1 node(int index) {
+		Node1 x = head;
 		
+		for(int i=0;i<index;i++) {
+			x = x.next;
+		}
+		return x;
+	}
+	
+	public void add(int k, Object input) {
+		if(k==0) {
+			addFirst(input);
+		}else{
+			Node1 temp1 = node(k-1);
+			Node1 temp2 = temp1.next;
+			Node1 newNode = new Node1(input);
+			temp1.next = newNode;
+			newNode.next = temp2;
+			size++;
+			if(newNode.next == null) {
+				tail=newNode;
+			}
+		}
 	}
 }
 
