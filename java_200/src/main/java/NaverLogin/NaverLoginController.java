4e5367825987222afc7d76f620b7d74247641a14
dahunkim <kim23222@naver.com>
2020-04-21 23:55:28 +0900

    [테스트] Model 인터페이스, swagger 테스트
    Model 인터페이스의 addAllAttributes 메서드와 containsAttribute 메서드 테스트
    ModelMap클래스 테스트

--- java_200/src/main/java/NaverLogin/NaverLoginController.java
+++ java_200/src/main/java/NaverLogin/NaverLoginController.java
@@ -1,29 +1,37 @@
 package NaverLogin;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.github.scribejava.core.model.OAuth2AccessToken;
 
+import aop.logExecution;
+
 @Controller
 public class NaverLoginController {
 		
 	
 		/* NaverLoginBO */
+		@Autowired
 		private NaverLoginBO naverLoginBO;
 		
-		@Autowired
-		private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
-			this.naverLoginBO = naverLoginBO;
-			
-		}
+//		private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
+//			this.naverLoginBO = naverLoginBO;
+//		}
+		
+		@logExecution
 		@RequestMapping("/login")
 	    public ModelAndView login(HttpSession session) {
 			System.out.println("확인");
@@ -39,5 +47,19 @@ public class NaverLoginController {
 			OAuth2AccessToken oauthToken = naverLoginBO.getAccessToken(session, code, state);
 			String apiResult = naverLoginBO.getUserProfile(oauthToken);
 			return new ModelAndView("callback", "result", apiResult);
+		}
+		
+		@RequestMapping("/test")
+		public String modeltest(Model model) {
+			Map<String, Integer> test = new HashMap<String, Integer>();
+			test.put("test1", 1);
+			test.put("test2", 2);
+			test.put("test3", 3);
+			test.put("test4", 4);
+			model.addAllAttributes(test);
+			if(model.containsAttribute("test1")) {
+				System.out.println("확인합니다. 테스트를 ! test1이 있습니다.");
+			}
+			return "mapTest";
 		}
 }
--- java_200/target/test-classes/java_200/aoptest.java
+++ java_200/target/test-classes/java_200/aoptest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -28,6 +29,9 @@ public class aoptest {
 	
 	@Inject
 	ApplicationContext applicationContext;
+	
+	@Autowired
+	private Environment environment;
 	
 	@Test
 	public void testt() throws Exception {
